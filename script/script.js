e27d5dca8f9feb199041f7bbe7f8c253f5a5ba86
soblin <hilo.soblin@gmail.com>
2021-01-02 07:12:51 +0900

    finished chapter3-1

--- script/script.js
+++ script/script.js
@@ -1,8 +1,13 @@
-const app = new Vue({
+new Vue({
     el: '#app',
     data() {
         return {
             text: 'hello, Vue.js'
         };
+    },
+    methods: {
+        handleClick: function() {
+            this.text = 'button pushed';
+        }
     }
 });
