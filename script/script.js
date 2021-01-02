1f80cba9c8a73faee04239fdfbbb8019a2829c37
soblin <hilo.soblin@gmail.com>
2021-01-02 09:57:44 +0900

    finished chapter4-5

--- script/script.js
+++ script/script.js
@@ -2,7 +2,22 @@ const app = new Vue({
     el: '#app',
     data() {
         return {
-            name: 'name'
+            name: 'name',
+            email: 'email',
+            tel: '00'
         };
+    },
+    computed: {
+        isInvalidName() {
+            return this.name.length < 4;
+        },
+        isInvalidEmail() {
+            const regex = new RegExp(/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/i);
+            return !regex.test(this.email);
+        },
+        isInvalidTel() {
+            const tel = this.tel;
+            return tel.length < 8 || isNaN(Number(tel));
+        }
     }
 });
